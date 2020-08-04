29940bb418a439520255c7a500228629163edbc1
Cyanic <hi.plexify@null.net>
2020-08-04 14:43:37 +0200

    those return; should do the work

--- script.js
+++ script.js
@@ -6,22 +6,25 @@ const passMedium = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 
 function gen() {
 
-  var basic = document.getElementById("basic");
-  var medium = document.getElementById("medium");
-  var hard = document.getElementById("hard");
-  var length = document.getElementById("length").value;
+  const basic = document.getElementById("basic");
+  const medium = document.getElementById("medium");
+  const hard = document.getElementById("hard");
+  const length = document.getElementById("length").value;
   
   if(basic.checked === true){
     
     if(isNaN(length)){
       alert("Length must be a number!");
+      return;
     }
     
     if(length > 2049 || length === 2049){
       alert("There are too many characters. The limit is 2048.")
+      return;
     }
     if(length < 8){
       alert("There's not enough characters! You can't go any lower than 8.")
+      return;
     }
     
     var basic_output = "";
@@ -29,6 +32,7 @@ function gen() {
     
     // show password
     document.getElementById("output").innerHTML = `${basic}`;
+    return;
     
   }
   
@@ -36,13 +40,16 @@ function gen() {
     
     if(isNaN(length)){
       alert("Length must be a number!");
+      return;
     }
     
     if(length > 2049 || length === 2049){
       alert("There are too many characters. The limit is 2048.")
+      return;
     }
     if(length < 8){
       alert("There's not enough characters! You can't go any lower than 8.")
+      return;
     }
     
     var medium_output = ""
@@ -51,6 +58,7 @@ function gen() {
     // show the final password.
     document.getElementById("output").innerHTML = `${medium_output}`
     document.getElementById("output").innerHTML = medium_output;
+    return;
     
   }
   
@@ -58,13 +66,16 @@ function gen() {
     
     if(isNaN(length)){
       alert("Length must be a number!");
+      return;
     }
     
     if(length > 2049 || length === 2049){
       alert("There are too many characters. The limit is 2048.")
+      return;
     }
     if(length < 8){
       alert("There's not enough characters! You can't go any lower than 8.")
+      return;
     }
     
     var hard_output = ""
@@ -73,6 +84,7 @@ function gen() {
     // show the final password
     document.getElementById("output").innerHTML = `${hard_output}`
     document.getElementById("output").innerHTML = hard_output;
+    return;
     
   }
 
@@ -80,12 +92,12 @@ function gen() {
 
 function copy() {
   var text = document.getElementById("output").innerHTML;
-  if(text === "Placeholder"){
+  if(text === "Placeholder" || !text){
     alert("Password is not generated!");
+    return;
   }
   navigator.clipboard.writeText(text)
   alert("Successfully copied the password to your clipboard!")
-  
 }
 
 // reveal
@@ -93,6 +105,7 @@ function reveal() {
   var out = document.getElementById("output");
   if(out.innerHTML === "Placeholder"){
     alert("Nothing to reveal.")
+    return;
   }
   out.style.color = "black";
 }
